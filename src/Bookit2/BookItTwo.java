ca7891b4fa886a8a20616d9a76c66ed2710cf2f5
fernando-boop <Bustafer-0078>
2020-02-29 15:51:19 -0500

    update BookItTwo

--- src/Bookit2/BookItTwo.java
+++ src/Bookit2/BookItTwo.java
@@ -6,5 +6,27 @@ public class BookItTwo {
 
 
         System.out.println("After sync");
+
+        /**
+         * My day to day activity:
+         * My shift starts at 8am, first thing first I double check the results of my smoke
+         * test, make sure that the application is up and running. Then I am focusing on my emails
+         * which I received yesterday after I left my desk, to make sure that i am note missing anything
+         * from yesterday. Then i am openning my jira board, basically getting ready for my daily stand
+         * up which occurs at 10am everyday. At stand up as a team we discuss what we did yesterday what
+         * we are working on right now and more importantly if there is anything that holding me to do my
+         * job, impediments. After stand up i go back to my desk and depending on which part of sprint our
+         * team is my tasks differ. lets say if it is early sprint i would participate in sprint planning
+         * meeting where we go over the user stories and pointing them. also I would work on maintaining my
+         * framework, participate in test scenario reviews. towards the mid sprint where the code is being
+         * released i would complete my automation and start execution. if it is end of the sprint I would be
+         * busy for getting reports ready and participate in demo meeting. Also I we have retro meeting at the
+         * end of the sprint. Every Tuesdays we have lunch and learn session where i share my coding and automation
+         * experience with my fellow manual testers, also every Friday we have code review meeting among automation
+         * testers of the project. As I am Java certificate I also participate in developers code review meetings
+         * just to get an idea f the code. So this
+         * is basically my day, oh i actually forgot about coffee :slightly_smiling_face: i do not start my day without
+         * a cup of coffee.
+         */
     }
 }
