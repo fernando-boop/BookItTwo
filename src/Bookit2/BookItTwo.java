fdb70a320c8551b29d1658fd5b0a5586f560bbed
fernando-boop <Bustafer-0078>
2020-02-29 15:22:38 -0500

    update BookItTwo

--- src/Bookit2/BookItTwo.java
+++ src/Bookit2/BookItTwo.java
@@ -3,5 +3,8 @@ package Bookit2;
 public class BookItTwo {
     public static void main(String[] args) {
         System.out.println("FErnando");
+
+
+        System.out.println("After sync");
     }
 }
